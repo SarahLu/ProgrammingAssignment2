79ce2e8a34c71590abea1b8d51a0e8c2e3f827c3
SarahLu <S_Stubenvoll@hotmail.com>
2015-01-25 00:58:05 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The function 'makecacheMatrix' provides cashing-functionality for the inverse of a 
+#matrix. 
 
-## Write a short comment describing this function
+#input: 'matrix' is a matrix for which the inverse is to be cached
 
-makeCacheMatrix <- function(x = matrix()) {
+#return: a list containing four functions named 'set', 'get', 'setinverse', 
+#'getinverse' to
+#    set the value of the matrix and automatcally reset the inverse-value to NULL
+#    get the value of the matrix
+#    set the value of the inverse
+#    get the value of the inverse
 
+makeCacheMatrix <- function(matrix = matrix()) {
+    
+    inverse <- NULL
+    
+    set <- function(inputMatrix) {
+        matrix <<- inputMatrix
+        inverse <<- NULL
+    }
+    
+    get <- function()  matrix
+    
+    setinverse <- function(inputInverse) inverse <<- inputInerse
+    
+    getinverse <- function() inverse
+    
+    list(set = set, 
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+#The function 'cacheSolve' provides the inverse of a matrix. The matrix must be 
+#invertable and wrapped by the 'makeCacheMatrix'-function. 'cacheSolve' returns a 
+#cached value if existant. Otherwise, it calculates and caches the inverse.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#input: 'matrixWrap' is an invertable matrix wrapped by the 'makeCacheMatrix'-function,  
+#i.e. a list of four functions named 'set', 'get', 'setinverse', 'getinverse' providing
+#access to a matrix and if existant its cached inverse.
+
+#output: the inverse of the input-matrix as a matrix
+
+cacheSolve <- function(matrixWrap, ...) {
+    
+    inv <- matrixWrap$getinverse()
+    
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    
+    matrix <- matrixWrap$get()
+    inv <- solve(matrix, ...)
+    matrixWrap$setinverse(inv)
+    
+    inv
 }
